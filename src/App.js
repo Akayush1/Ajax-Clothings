62c605e67fb5aaa728d4f12518a62a8a860d3ffd
Ayush <Akayushfirst@gmail.com>
2024-08-20 10:55:39 -0400

    First Commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import { Navbar } from './componenets/Navbar/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { Shop } from './pages/Shop';
+import { ShopCategory } from './pages/ShopCategory';
+import { Product } from './pages/Product';
+import { Cart } from './pages/Cart';
+import { LoginSignup } from './pages/LoginSignup';
+import { Footer } from './componenets/Footer/Footer';
+import men_banner from './componenets/Assets/banner_mens.png'
+import women_banner from './componenets/Assets/banner_women.png'
+import kids_banner from './componenets/Assets/banner_kids.png'
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+     <Navbar/>
+     <Routes>
+      <Route path='/' element={<Shop/>}/>
+      <Route path='/mens' element={<ShopCategory banner={men_banner} category="mens"/>}/>
+      <Route path='/womens' element={<ShopCategory banner={women_banner} category="womens"/>}/>
+      <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kids"/>}/>
+      <Route path='/product' element={<Product/>}>
+      <Route path=':productId' element={<Product/>}/>
+</Route>
+<Route path='/cart' element={<Cart/>}/>
+<Route path='/login' element={<LoginSignup/>}/>
+
+
+
+     </Routes>
+     <Footer/>
+     </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Shop.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Hero} from '../componenets/Hero/Hero'
+import { Popular } from '../componenets/Popular/Popular'
+import { Offers } from '../componenets/Offers/Offers'
+import { NewCollections } from '../componenets/NewCollections/NewCollections'
+import { NewsLetter } from '../componenets/NewsLetter/NewsLetter'
+
+export const Shop = () => {
+  return (
+    <div>
+      <Hero/>
+      <Popular/>
+      <Offers/>
+      <NewCollections/>
+      <NewsLetter/>
+
+
+    </div>
+  )
+}
